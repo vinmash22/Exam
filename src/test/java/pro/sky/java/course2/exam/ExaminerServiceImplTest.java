0b2fdc35c829ebd2f861612dfd9d2af4cbe4ac17
vinmash <vinmash@mail.ru>
2023-06-19 18:42:02 +0500

    6

--- src/test/java/pro/sky/java/course2/exam/ExaminerServiceImplTest.java
+++ src/test/java/pro/sky/java/course2/exam/ExaminerServiceImplTest.java
@@ -6,7 +6,9 @@ import org.mockito.Mock;
 import org.mockito.MockitoAnnotations;
 
 import java.util.Collection;
+import java.util.HashSet;
 import java.util.List;
+import java.util.Set;
 
 import static org.junit.jupiter.api.Assertions.*;
 import static org.mockito.Mockito.when;
@@ -28,7 +30,7 @@ class ExaminerServiceImplTest {
         List<Question> questions = List.of(
 
                 new Question("1", "1"),
-                new Question("1", "1"));
+                new Question("2", "2"));
         when(javaQuestionServiceMock.getAll()).thenReturn(questions);
         assertThrows(RequestOverSizeException.class, () -> service.getQuestions(3));
     }
@@ -39,6 +41,30 @@ class ExaminerServiceImplTest {
         Question actual = new Question("Вопрос1", "Ответ1");
         when(javaQuestionServiceMock.getRandomQuestion()).thenReturn(actual);
         assertEquals(expected, actual);
+
+    }
+
+    @Test
+    void getQuestionsTest1() {
+        List<Question> questions = List.of(
+
+                new Question("1", "1"),
+                new Question("2", "2"));
+        when(javaQuestionServiceMock.getAll()).thenReturn(questions);
+        Collection<Question> questionSet = service.getQuestions(2);
+        assertEquals(2, questionSet.size());
+
+    }
+
+    @Test
+    void getQuestionsTest2() {
+        List<Question> questions = List.of(
+                new Question("1", "1"),
+                new Question("2", "2"));
+        when(javaQuestionServiceMock.getAll()).thenReturn(questions);
+        when(javaQuestionServiceMock.getRandomQuestion()).thenReturn(new Question("1", "1"), new Question("2", "2"));
+        var actual = service.getQuestions(2);
+        assertEquals(2, actual.size());
 
     }
 
